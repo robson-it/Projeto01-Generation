f3680b9652644edaf716edaebf825a25d5e4bb5b
Robson A. Rocha <robson_it@outlook.com>
2023-09-11 16:41:48 -0300

    Implementação da controler e chamada dos métodos no menu

---
+++ ProjetoFinalBloco01/Controller/CelularController.cs
@@ -0,0 +1,81 @@
+﻿using ProjetoFinalBloco01.Model;
+using ProjetoFinalBloco01.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProjetoFinalBloco01.Controller
+{
+    public class CelularController : ICelularRepository
+    {
+        private readonly List<Celular> listaCelulares = new List<Celular>();
+        private int numeroCodigoCelular = 1;
+
+        public void AtualizarAparelho(Celular celular)
+        {
+            var buscarCelular = BuscarAparelho(celular.getCodigoCelular());
+            if (buscarCelular is not null)
+            {
+                var indice = listaCelulares.IndexOf(buscarCelular);
+                listaCelulares[indice] = celular;
+                Console.Clear();
+                Console.WriteLine($"\nO aparelho celular (Código: {celular.getCodigoCelular()}) foi atualizado com sucesso!");
+            }
+        }
+
+        public void CadastrarAparelho(Celular celular)
+        {
+            listaCelulares.Add(celular);
+            Console.Clear();
+            Console.WriteLine($"\nO aparelho celular (Código: {celular.getCodigoCelular()}) foi cadastrado com sucesso!");
+        }
+
+        public void DeletarAparelho(int codigoAparelho)
+        {
+            var celular = BuscarAparelho(codigoAparelho);
+
+            if (celular is not null)
+            {
+                if (listaCelulares.Remove(celular))
+                {
+                    Console.Clear();
+                    Console.WriteLine($"\nO aparelho celular (Código: {celular.getCodigoCelular()}) foi removido com sucesso!");
+                };
+            }
+            else
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\nO aparelho celular (Código: {celular.getCodigoCelular()}) não foi encontrado!");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+            }
+
+        }
+
+        public void ListarAparelhos()
+        {
+            foreach (var celular in listaCelulares) {
+                celular.visualizarAparelho();
+            }
+        }
+
+        public Celular? BuscarAparelho(int codigoAparelho) {
+
+            foreach (var celular in listaCelulares) { 
+                if(celular.getCodigoCelular() == codigoAparelho)
+                {
+                    return celular;
+                }  
+            }
+            return null;
+        }
+
+        public int GerarCodigoCelular()
+        {
+            return numeroCodigoCelular++;
+        }
+    }
+}
--- ProjetoFinalBloco01/Program.cs
+++ ProjetoFinalBloco01/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.ComponentModel.Design;
+﻿using ProjetoFinalBloco01.Controller;
+using ProjetoFinalBloco01.Model;
+using System;
+using System.ComponentModel.Design;
+using System.Data;
 
 namespace ProjetoFinalBloco01
 {
@@ -6,8 +10,11 @@ namespace ProjetoFinalBloco01
     {
         static void Main(string[] args)
         {
-            int opcaoMenu, imei;
-            string modelo, fabricante, sistemaOperacional, cor, descricao;
+            int opcaoMenu, imei, codigoAparelho;
+            string modelo, fabricante, sistemaOperacional, cor, descricao, leitorBiometrico, frequenciaRedeMovel;
+            decimal preco;
+             
+            CelularController celularController = new CelularController();
 
             while (true)
             {
@@ -38,42 +45,308 @@ namespace ProjetoFinalBloco01
                         Console.Clear();
                         Console.WriteLine("");
                         Console.WriteLine("[CADASTRAR NOVO APARELHO]\n");
+
                         Console.WriteLine("Digite o modelo do aparelho: ");
                         modelo = Console.ReadLine();
                         modelo ??= string.Empty;
+                        if (modelo.Equals(string.Empty))
+                        {
+                            var opcao = "";
+                            Console.WriteLine("O modelo do celular não foi inserido! [M] Continuar [C] Cancelar!");
+                            opcao = Console.ReadLine().ToUpper();
+                            if (opcao.Equals("C"))
+                            {
+                                CancelarOperacao();
+                                break;
+                            }
+                            else if (!opcao.Equals("M"))
+                            {
+                                CancelarOperacao("Operação inválida! ");
+                                break;
+                            }       
+
+                        }
+
                         Console.WriteLine("Digite o nome do fabricante: ");
                         fabricante = Console.ReadLine();
                         fabricante ??= string.Empty;
+
+                        if (fabricante.Equals(string.Empty))
+                        {
+                            var opcao = "";
+                            Console.WriteLine("O fabricante do celular não foi inserido! [M] Continuar [C] Cancelar!");
+                            opcao = Console.ReadLine().ToUpper();
+                            if (opcao.Equals("C"))
+                            {
+                                CancelarOperacao();
+                                break;
+                            }
+                            else if (!opcao.Equals("M"))
+                            {
+                                CancelarOperacao("Operação inválida! ");
+                                break;
+                            }
+
+                        }
+
                         Console.WriteLine("Digite o nome do Sistema Operacional: ");
                         sistemaOperacional = Console.ReadLine();
                         sistemaOperacional ??= string.Empty;
+
+                        if (sistemaOperacional.Equals(string.Empty))
+                        {
+                            var opcao = "";
+                            Console.WriteLine("O sistema operacional do celular não foi inserido! [M] Continuar [C] Cancelar!");
+                            opcao = Console.ReadLine().ToUpper();
+                            if (opcao.Equals("C"))
+                            {
+                                CancelarOperacao();
+                                break;
+                            }
+                            else if (!opcao.Equals("M"))
+                            {
+                                CancelarOperacao("Operação inválida! ");
+                                break;
+                            }
+
+                        }
+
                         Console.WriteLine("Digite a cor do aparelho: ");
                         cor = Console.ReadLine();
                         cor ??= string.Empty;
+
+                        if (cor.Equals(string.Empty))
+                        {
+                            var opcao = "";
+                            Console.WriteLine("A cor do celular não foi inserida! [M] Continuar [C] Cancelar!");
+                            opcao = Console.ReadLine().ToUpper();
+                            if (opcao.Equals("C"))
+                            {
+                                CancelarOperacao();
+                                break;
+                            }
+                            else if (!opcao.Equals("M"))
+                            {
+                                CancelarOperacao("Operação inválida! ");
+                                break;
+                            }
+
+                        }
+
                         Console.WriteLine("Digite uma descrição para o aparelho (Ex: memória, resolução): ");
                         descricao = Console.ReadLine();
                         descricao ??= string.Empty;
-                        Console.WriteLine("Digite o IMEI aparelho: ");
-                        imei = Convert.ToInt32(Console.ReadLine());
-                        break;
 
+                        if (descricao.Equals(string.Empty))
+                        {
+                            var opcao = "";
+                            Console.WriteLine("A descricao do celular não foi inserida! [M] Continuar [C] Cancelar!");
+                            opcao = Console.ReadLine().ToUpper();
+                            if (opcao.Equals("C"))
+                            {
+                                CancelarOperacao();
+                                break;
+                            }
+                            else if (!opcao.Equals("M"))
+                            {
+                                CancelarOperacao("Operação inválida! ");
+                                break;
+                            }
+
+                        }
+
+                        do
+                        {
+                            try
+                            {
+                                Console.WriteLine("Digite o IMEI aparelho: ");
+                                imei = Convert.ToInt32(Console.ReadLine());
+                                if(imei <= 0)
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("\n o Número do IMEI deve ser maior que 0 | Tente novamente!");
+                                    KeyPress();
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                                
+                            }catch(Exception e)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("\n O Número do IMEI deve ser um número | Tente novamente!");
+                                KeyPress();
+                            }
+                            
+                        } while (true);
+
+
+
+                        Console.WriteLine("O aparelho possui leitor biométrico? [S] sim [N] não");
+                        leitorBiometrico = Console.ReadLine().ToUpper();
+                        leitorBiometrico ??= string.Empty;
+
+                        if ((leitorBiometrico.Equals(string.Empty)) )
+                        {
+                            var opcao = "";
+                            Console.WriteLine("Não foi inserida nenhuma resposta para leitor biomátrico! [M] Manter padrão \"não\" [C] Cancelar!");
+                            opcao = Console.ReadLine().ToUpper();
+                            if (opcao.Equals("C"))
+                            {
+                                CancelarOperacao();
+                                break;
+                            }
+                            else if (!opcao.Equals("M"))
+                            {
+                                leitorBiometrico = "N";
+
+                            }
+                            else
+                            {
+                                leitorBiometrico = "N";
+                            }
+
+                        }else if ((!leitorBiometrico.Equals("S") && !leitorBiometrico.Equals("M"))) {
+                            Console.WriteLine("Opção inválida! Será adotado por padrão \"não\"");
+                            leitorBiometrico = "N";
+                        }
+
+                        Console.WriteLine("Digite a tecnologia de internet (3G / 4G / 5G : ");
+                        frequenciaRedeMovel = Console.ReadLine();
+                        frequenciaRedeMovel ??= string.Empty;
+
+                        if (frequenciaRedeMovel.Equals(string.Empty))
+                        {
+                            var opcao = "";
+                            Console.WriteLine("Nenhuma tecnologia digitada! [M] Continuar [C] Cancelar!");
+                            opcao = Console.ReadLine().ToUpper();
+                            if (opcao.Equals("C"))
+                            {
+                                CancelarOperacao();
+                                break;
+                            }
+                            else if (!opcao.Equals("M"))
+                            {
+                                CancelarOperacao("Operação inválida! ");
+                                break;
+                            }
+
+                        }
+
+
+                        do
+                        {
+                            try
+                            {
+                                Console.WriteLine("Digite o preço do aparelho R$: ");
+                                preco = Convert.ToDecimal(Console.ReadLine());
+                                if (preco <= 0)
+                                {
+                                    Console.Clear();
+                                    Console.WriteLine("\n O preço do aparelho deve ser maior que 0 | Tente novamente!");
+                                    KeyPress();
+                                }
+                                else
+                                {
+                                    break;
+                                }
+
+                            }
+                            catch (Exception e)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("\n Valor inválido | Tente novamente!");
+                                KeyPress();
+                            }
+
+                        } while (true);
+
+                        if (leitorBiometrico.Equals("N")){
+                            celularController.CadastrarAparelho(new SmartPhone(
+                                celularController.GerarCodigoCelular(), modelo, fabricante, sistemaOperacional, cor, descricao, imei, preco, false, frequenciaRedeMovel));
+                        }
+                        else
+                        {
+                            celularController.CadastrarAparelho(new SmartPhone(
+                                celularController.GerarCodigoCelular(), modelo, fabricante, sistemaOperacional, cor, descricao, imei, preco, true, frequenciaRedeMovel));
+                        }
+                        KeyPress();
+                        break;
                     case 2:
                         
                         Console.Clear();
                         Console.WriteLine("");
                         Console.WriteLine("[ATUALIZAR DESCRIÇÃO DO APARELHO]");
-                        Console.WriteLine("Digite uma descrição para o aparelho (Ex: memória, resolução): ");
-                        descricao = Console.ReadLine();
+
+                        try
+                        {
+                            Console.WriteLine("\n\nDigite código do aparelho:");
+                            opcaoMenu = Convert.ToInt32(Console.ReadLine());
+
+                            var celular = celularController.BuscarAparelho(opcaoMenu);
+
+                            if(celular is not null)
+                            {
+                                Console.WriteLine("Digite uma descrição para o aparelho (Ex: memória, resolução): ");
+                                descricao = Console.ReadLine();
+                                descricao ??= string.Empty;
+
+                                if (descricao.Equals(string.Empty))
+                                {
+                                    var opcao = "";
+                                    Console.WriteLine("A descricao do celular não foi inserida! [M] Continuar [C] Cancelar!");
+                                    opcao = Console.ReadLine().ToUpper();
+                                    if (opcao.Equals("C"))
+                                    {
+                                        CancelarOperacao();
+                                        break;
+                                    }
+                                    else if (!opcao.Equals("M"))
+                                    {
+                                        CancelarOperacao("Operação inválida! ");
+                                        break;
+                                    }
+
+                                }
+
+                                celular.setDescricao(descricao);
+                                celularController.AtualizarAparelho(celular);
+                            }
+                            
+
+                        }
+                        catch (FormatException e)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Clear();
+                            Menu();
+                            Console.WriteLine("Número inválido!");
+                            Console.ResetColor();
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            opcaoMenu = -1;
+                        }
+
+                        
+
+
+                        KeyPress();
                         break;
                     case 3:
                         Console.Clear();
                         Console.WriteLine("");
                         Console.WriteLine("[DELETAR APARELHO CADASTRADO]");
+                        Console.WriteLine("Digite o código do aparelho que deseja deletar: ");
+                        codigoAparelho = Convert.ToInt32(Console.ReadLine());
+                        celularController.DeletarAparelho(codigoAparelho);
+                        KeyPress();
                         break;
                     case 4:
                         Console.Clear();
                         Console.WriteLine("");
                         Console.WriteLine("[LISTAR APARELHOS CADASTRADOS]");
+                        celularController.ListarAparelhos();
+                        KeyPress();
                         break;
                     default:
                         Console.WriteLine("Opção inválida! Tente novamente!");
@@ -97,6 +370,25 @@ namespace ProjetoFinalBloco01
         {
 
             return numeroInteiro;
+        }
+
+        private static ConsoleKeyInfo consoleKeyInfo;
+        static void KeyPress()
+        {
+            do
+            {
+                Console.Write("\nPressione Enter para Continuar...");
+                consoleKeyInfo = Console.ReadKey();
+            } while (consoleKeyInfo.Key != ConsoleKey.Enter);
+        }
+
+        static void CancelarOperacao(string invalida = null)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\n" + invalida + "Operação cancelada com sucesso!");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            KeyPress();
         }
     }
 }
